1f6c157ef7f56f103de1453f6368863507f272d8
Fulvio Frapolli <fulvio.frapolli@edu.ti.ch>
2024-10-28 17:29:56 +0100

    allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)rh1p)f6*54zjs$muasbsemx10_f+^%f6o%5&h5es76get(7q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
