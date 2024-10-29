5e49cbfcd6a2b5ce8f063d76d16e4a835a598ec1
Fulvio Frapolli <fulvio.frapolli@edu.ti.ch>
2024-10-29 09:58:13 +0100

    test with db password

--- django_project/settings.py
+++ django_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "password",
         "HOST": "db",  # set in docker-compose.yml
         "PORT": 5432,  # default postgres port
     }
